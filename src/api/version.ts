80a05ecf9df23360ba7ebad98951bab43b4c918e
Oscar Mui <oscarmui1127@gmail.com>
2023-08-10 11:25:22 +0800

    v20230809.2, added timing check

--- src/api/version.ts
+++ src/api/version.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
-import {ACCEPTED_CLIENT_VERSIONS} from "../common/constants";
+import {ACCEPTED_CLIENT_VERSIONS, NETWORK_DELAY_MS} from "../common/constants";
 
 export default (req : Request,res : Response) => {
     res.send({
         result: "success",
         acceptedClientVersions: ACCEPTED_CLIENT_VERSIONS,
+        currentTime: Date.now(),
+        allowedNetworkDelay: NETWORK_DELAY_MS,
     })
 };
