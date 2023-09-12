94bcc08dbd266092f8893f89c23e78c5a03ef98b
Oscar Mui <oscarmui1127@gmail.com>
2023-09-12 17:40:44 +0800

    v20230912.0, updated constant values

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,14 +1,14 @@
-const production = false
+const production = true
 
 // Constants mainly for testing purposes
-export const PARTICIPANTS_PER_GAME = production ? 5 : 2;
-export const DEAD_LIMIT = production ? -10 : -3;
+export const PARTICIPANTS_PER_GAME = production ? 5 : 5;
+export const DEAD_LIMIT = production ? -5 : -3;
 
 //tolerance time to network delays, the client does not know about this
 export const NETWORK_DELAY_MS = production ? 1000 : 1000;
 
 //time allowed per round
-export const ROUND_TIME_MS = production ? 3 * 60 * 1000 : 60 * 1000; //180s
+export const ROUND_TIME_MS = production ? 2 * 60 * 1000 : 60 * 1000; //180s
 
 //time allowed when every player has made their decision
 export const SHORTENED_TIME_MS = production ? 15 * 1000 : 15 * 1000; //15s
@@ -17,9 +17,9 @@ export const SHORTENED_TIME_MS = production ? 15 * 1000 : 15 * 1000; //15s
 export const ROUND_ZERO_DIGEST_TIME_MS = 2 * 1000; //2s
 
 //time given to players between rounds to digest information, the client does not know about this
-export const ROUND_INFO_DIGEST_TIME_MS = 7 * 1000; //7s
+export const ROUND_INFO_DIGEST_TIME_MS = 10 * 1000; //7s
 
 //time given to players to digest other normal information, the client does not know about this
 export const DIGEST_TIME_MS = 5 * 1000; //5s
 
-export const ACCEPTED_CLIENT_VERSIONS = ["20230811.dev"]
+export const ACCEPTED_CLIENT_VERSIONS = ["20230912.0"]
