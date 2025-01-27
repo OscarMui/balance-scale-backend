25c0d08eddaaf42ce55aeeb31ccfbe9b13ca0dec
Oscar Mui <oscarmui1127@gmail.com>
2025-01-27 09:56:17 +0000

    wrapping more firebase code in try catch

--- src/common/firestore.ts
+++ src/common/firestore.ts
@@ -2,14 +2,13 @@ import admin from 'firebase-admin';
 import { Statistic } from './interfaces';
 
 const createStatistic = async (data: Statistic) => {
-
-  const db = admin.firestore();
-    try {
-      const docRef = await db.collection('statisticsV1').add(data);
-      console.log('Document written with ID:', docRef.id, 'to statisticsV1');
-    } catch (error) {
-      console.error('Error adding document to statisticsV1:', error);
-    }
+  try {
+    const db = admin.firestore();
+    const docRef = await db.collection('statisticsV1').add(data);
+    console.log('Document written with ID:', docRef.id, 'to statisticsV1');
+  } catch (error) {
+    console.error('Error adding document to statisticsV1:', error);
   }
+}
 
 export {createStatistic};
